45c1f04d4349d72248603853ac976c4f57f2fb2c
Grace Choi <oiograceoio@gmail.com>
2019-03-21 15:55:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,9 +20,9 @@ class Dog
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE dogs
-      SQL
-    DB[:conn].execute(sql)
+#    sql = <<-SQL
+#      DROP TABLE dogs
+#      SQL
+    DB[:conn].execute("DROP TABLE dogs")
   end
 end
