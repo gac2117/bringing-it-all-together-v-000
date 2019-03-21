fb8143abfc8f17fa83cf61e242d0eafb85b5ef4b
Grace Choi <oiograceoio@gmail.com>
2019-03-21 16:25:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,9 @@ class Dog
       SELECT * FROM dogs
       WHERE id = ? LIMIT 1
       SQL
-    DB[:conn].execute(sql, id)
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
