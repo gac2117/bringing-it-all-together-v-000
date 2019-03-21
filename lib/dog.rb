a875049f24ff119354ff3742ad0aa54c5ec9697d
Grace Choi <oiograceoio@gmail.com>
2019-03-21 16:35:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,8 @@ class Dog
       WHERE id = ?
       SQL
     dog = DB[:conn].execute(sql, id)[0]
-    Dog.new(dog[0], dog[1], dog[2])
+    Dog.new(dog[0], dog[1], dog[2])  
+    self
   end
 
   def save
