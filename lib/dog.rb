32a08687f3aff704bcc4b0962d91c6048fe52780
Grace Choi <oiograceoio@gmail.com>
2019-03-21 16:29:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -30,7 +30,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new
+    new_dog = self.new(name: name, breed: breed)
     new_dog.id = row[0]
     new_dog.name = row[1]
     new_dog.breed = row[2]
