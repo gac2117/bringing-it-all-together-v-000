fbda2c2b29e9fbb4425bda8d89c134e53776a023
Grace Choi <oiograceoio@gmail.com>
2019-03-21 16:21:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,16 @@ class Dog
     end.first
   end
 
+  def self.find_by_id(id)
+    sql=<<-SQL
+      SELECT * FROM dogs 
+      WHERE id = ? LIMIT 1
+      SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def save
     if self.id
       self.update
