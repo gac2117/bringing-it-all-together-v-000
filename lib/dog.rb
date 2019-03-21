45b888fd5c07d65eae252b6219c26f589c71b4c9
Grace Choi <oiograceoio@gmail.com>
2019-03-21 15:47:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   attr_accessor :name, :breed
-  attr_reader :id 
+  attr_reader :id
 
   def initialize(id=nil, name, breed)
-    @id = id 
-    @name = name 
-    @breed = breed 
+    @id = id
+    @name = name
+    @breed = breed
   end
 end
